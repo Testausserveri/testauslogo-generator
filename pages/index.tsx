45aa2253ea657fe6c0172d01318991f69f0ee9e6
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-06-22 22:45:43 +0300

    Improve styles, add Testausserveri logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useState } from 'react'
+import { TestausserveriLogo } from '../components/TestausserveriLogo';
 
 export default function Home() {
   const [text, setText] = useState("");
+  const router = useRouter();
 
   return (
     <div className="container">
@@ -11,14 +14,31 @@ export default function Home() {
         <title>Testauslogo generator</title>
         <meta name="description" content="Generate logos for projects related to Testausserveri" />
       </Head>
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Enter text"
-      />
-      <Link href={`/${text}`}>
-        Generate
-      </Link>
+      <main>
+        <div className="texts">
+          <h1>Testauslogo Generator</h1>
+          <p>Generate logos for Testausserveri projects</p>
+        </div>
+        <form onSubmit={() => {
+          router.push(`/${text}`);
+        }}>
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text"
+          />
+          <button type="submit" className="generationButton">
+            Generate
+          </button>
+        </form>
+      </main>
+      <a href="https://testausserveri.fi" style={{
+        position: "absolute",
+        left: 40,
+        bottom: 20
+      }}>
+        <TestausserveriLogo />
+      </a>
     </div >
   )
 }
