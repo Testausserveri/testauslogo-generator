d4ab135c1b3c1ff2706519a2198361a41129032c
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-03-15 11:56:31 +0200

    Add Dockerfile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react'
 import { TestausserveriLogo } from '../components/TestausserveriLogo';
